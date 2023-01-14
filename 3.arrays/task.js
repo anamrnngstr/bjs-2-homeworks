06a8954c131f4fab8b6318ccf35d2452178dea5d
Ana Mrnngstr <tyurinaana666@gmail.com>
2023-01-14 21:40:11 +0300

    third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.every((el, ind, arr1) => arr1[ind] === arr2[ind] && arr1.length === arr2.length )) {
+          return true 
+        } else {
+          return false
+        }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(el => el.gender === gender).map(el => el.age).reduce((a, b, ind, arr) => {
+        a += b;
+        if (ind === arr.length - 1) {
+          return a / arr.length;
+        }
+        return a
+      }, 0);
 }
